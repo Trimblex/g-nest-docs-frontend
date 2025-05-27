c3149bb36ed64ba4f47419da3279d2072696c551
薛天宝 <10551402+xue-tianbao@user.noreply.gitee.com>
2025-05-27 21:29:15 +0800

    本地初次开发完成

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,49 @@
 import type { NextConfig } from "next";
 
+const getAutoBaseUrl = () => {
+  // 本地开发强制使用 localhost
+  if (process.env.NODE_ENV === "development") {
+    return "http://localhost:3000";
+  }
+  // 获取 Vercel 全环境统一域名
+  const vercelUrl = process.env.VERCEL_URL;
+  // 安全回退机制
+  if (!vercelUrl) {
+    console.warn("VERCEL_URL not detected, fallback to localhost");
+    return "http://localhost:3000";
+  }
+  // 自动补全 HTTPS 协议（VERCEL_URL 可能不带协议）
+  return vercelUrl.startsWith("http") ? vercelUrl : `https://${vercelUrl}`;
+};
 const nextConfig: NextConfig = {
+  // transpilePackages: ["docx", "prosemirror-docx"],
   /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/:path*", // 匹配所有路径
+        destination: `/:path*`, // 保持路径一致性
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "47.109.205.159",
+        port: "9000",
+        pathname: "/**",
+      },
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_URL: getAutoBaseUrl(),
+  },
 };
 
 export default nextConfig;
