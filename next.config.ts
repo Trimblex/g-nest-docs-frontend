06fc3edecb7f10ca0d4098cf063636f69580cfe7
薛天宝 <10551402+xue-tianbao@user.noreply.gitee.com>
2025-05-28 15:10:42 +0800

    修改环境配置

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,5 @@
 import type { NextConfig } from "next";
 
-const getAutoBaseUrl = () => {
-  // 本地开发强制使用 localhost
-  if (process.env.NODE_ENV === "development") {
-    return "http://localhost:3000";
-  } else if (process.env.NODE_ENV === "production") {
-    return "http://162.14.126.179";
-  }
-  // 获取 Vercel 全环境统一域名
-  const vercelUrl = process.env.VERCEL_URL;
-  // 安全回退机制
-  if (!vercelUrl) {
-    console.warn("VERCEL_URL not detected, fallback to localhost");
-    return "http://localhost:3000";
-  }
-  // 自动补全 HTTPS 协议（VERCEL_URL 可能不带协议）
-  return vercelUrl.startsWith("http") ? vercelUrl : `https://${vercelUrl}`;
-};
 const nextConfig: NextConfig = {
   // transpilePackages: ["docx", "prosemirror-docx"],
   /* config options here */
@@ -42,9 +25,6 @@ const nextConfig: NextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_URL: getAutoBaseUrl(),
   },
 };
 
