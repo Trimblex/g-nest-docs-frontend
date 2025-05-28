1b7d34f53264f835c7de8758f347089094b5047f
薛天宝 <10551402+xue-tianbao@user.noreply.gitee.com>
2025-05-28 11:19:20 +0800

    修改配置

---
+++ src/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "g-nest-frontend",
+      script: "npm",
+      args: "start",
+      cwd: "/home/js/g-nest-frontend",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
